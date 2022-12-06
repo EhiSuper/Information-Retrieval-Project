074c37da55b8dabc724a3967319e349a34f37a0d
ImBadnick <fuma2@hotmail.it>
2022-12-06 16:05:29 +0100

    Added non ascii chars removal

--- src/main/java/it/unipi/dii/aide/mircv/InformationRetrievalProject/TextPreprocessing/TextPreprocessing.java
+++ src/main/java/it/unipi/dii/aide/mircv/InformationRetrievalProject/TextPreprocessing/TextPreprocessing.java
@@ -2,13 +2,20 @@ package it.unipi.dii.aide.mircv.InformationRetrievalProject.TextPreprocessing;
 
 
 public class TextPreprocessing {
-    static Boolean STEMMING_AND_STOPWORDS = false;
+    static Boolean REMOVE_STOPWORDS= true;
+    static Boolean STEMMING= false;
     public static String parse(String document){
-        document = document.toLowerCase().replaceAll("\\p{Punct}", " ");
-        if (STEMMING_AND_STOPWORDS){
-            document = StopWordsRemover.removeStopWords(document);
-            document = Stemmer.stemming(document);
+        document = document.toLowerCase(); //Lowercase text
+        document = document.replaceAll("\\p{Punct}", " "); //Remove punctualization
+        document = document.replaceAll("[^\\x00-\\x7F]", ""); //Remove non-ascii chars
+
+        if(REMOVE_STOPWORDS){
+            document = StopWordsRemover.removeStopWords(document); //Remove stopwords
         }
+        if(STEMMING){
+            document = Stemmer.stemming(document); //Apply stemming
+        }
+
         return document;
     }
 }
